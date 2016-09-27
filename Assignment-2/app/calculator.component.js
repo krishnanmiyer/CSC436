bcd8765f59762d571e7f9d07e973860600668bb7
krishnanmiyer <krishnan_mahadevan@outlook.com>
2016-09-27 01:59:15 -0700

    updated code without key events

--- Assignment-2/app/calculator.component.js
+++ Assignment-2/app/calculator.component.js
@@ -9,19 +9,46 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var MathValue = (function () {
-    function MathValue() {
-    }
-    return MathValue;
-}());
-exports.MathValue = MathValue;
 var CalculatorComponent = (function () {
     function CalculatorComponent() {
     }
     CalculatorComponent.prototype.clicked = function (event) {
-        console.log(event.srcElement.attributes.type == 'button');
-        if (event.srcElement.attributes.type == 'button') {
-            console.log("button clicked");
+        if (event.target.type == 'button') {
+            if (this.calculator == undefined) {
+                this.calculator = new Calculator();
+            }
+            var v = event.target.innerText;
+            if (this.isNumber(v)) {
+                this.calculator.setValue(v);
+            }
+            else if (this.isOperator(v)) {
+                this.calculator.setOperator(v);
+            }
+            else if (v == '=') {
+                this.display = String(this.calculator.calculate());
+                return;
+            }
+            else {
+                this.calculator.clear();
+            }
+            this.display = this.calculator.toString();
+        }
+    };
+    CalculatorComponent.prototype.onKey = function (event) {
+        console.log("---->" + event.target.value);
+    };
+    CalculatorComponent.prototype.isNumber = function (n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+    CalculatorComponent.prototype.isOperator = function (o) {
+        switch (o) {
+            case '+':
+            case '-':
+            case '/':
+            case 'x':
+                return true;
+            default:
+                return false;
         }
     };
     CalculatorComponent = __decorate([
@@ -35,4 +62,63 @@ var CalculatorComponent = (function () {
     return CalculatorComponent;
 }());
 exports.CalculatorComponent = CalculatorComponent;
+var Calculator = (function () {
+    function Calculator() {
+        this._oldValue = undefined;
+        this._newValue = undefined;
+        this._operator = "";
+    }
+    Calculator.prototype.toString = function () {
+        if (this._operator.length == 0) {
+            return String(this._oldValue);
+        }
+        else if (this._operator.length > 0 && this._newValue == undefined) {
+            return this._oldValue + ' ' + this._operator;
+        }
+        else {
+            return this._oldValue + ' ' + this._operator + ' ' + this._newValue;
+        }
+    };
+    Calculator.prototype.clear = function () {
+        this._oldValue = undefined;
+        this._newValue = undefined;
+        this._operator = "";
+        this.setValue(0);
+    };
+    Calculator.prototype.setValue = function (v) {
+        if (this._operator.length == 0) {
+            this._oldValue = (this._oldValue == undefined || this._oldValue == 0) ? v : this._oldValue + v;
+        }
+        else {
+            this._newValue = (this._newValue == undefined || this._newValue == 0) ? v : this._newValue + v;
+        }
+    };
+    Calculator.prototype.setOperator = function (v) {
+        this._operator = v;
+    };
+    Calculator.prototype.calculate = function () {
+        if (this._oldValue == undefined || this._newValue == undefined || this._operator.length == 0) {
+            return 0;
+        }
+        switch (this._operator) {
+            case '+':
+                this._oldValue = Number(this._oldValue) + Number(this._newValue);
+                break;
+            case '-':
+                this._oldValue -= this._newValue;
+                break;
+            case '/':
+                this._oldValue /= this._newValue;
+                break;
+            case 'x':
+                this._oldValue *= this._newValue;
+                break;
+        }
+        this._newValue = undefined;
+        this._operator = "";
+        return this._oldValue;
+    };
+    return Calculator;
+}());
+exports.Calculator = Calculator;
 //# sourceMappingURL=calculator.component.js.map
--- Assignment-2/app/calculator.component.ts
+++ Assignment-2/app/calculator.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
 
-export class MathValue {
-  oldValue: number;
-  newValue: number;
-  operatorValue: string;
-}
-
 @Component({
   selector: 'calculator',
   templateUrl: 'app/calculator.component.html',
@@ -13,10 +7,115 @@ export class MathValue {
 })
 
 export class CalculatorComponent {
-   clicked(event) {
-    console.log(event.srcElement.attributes.type == 'button');
-    if (event.srcElement.attributes.type == 'button') {
-      console.log("button clicked");
+  calculator: Calculator;
+  display: string;
+
+  clicked(event) {
+    if (event.target.type == 'button') {
+
+      if (this.calculator == undefined) {
+        this.calculator = new Calculator();
+      }
+
+      let v = event.target.innerText;
+
+      if (this.isNumber(v)) {
+        this.calculator.setValue(v);
+      }
+      else if (this.isOperator(v)) {
+        this.calculator.setOperator(v);
+      }
+      else if (v == '=') {
+        this.display = String(this.calculator.calculate());
+        return;
+      }
+      else {
+        this.calculator.clear();
+      }
+      this.display = this.calculator.toString();
     }
+  }
+
+  onKey(event:KeyboardEvent) {
+    console.log("---->" + (<HTMLInputElement>event.target).value);
+  }
+
+  isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  isOperator(o) {
+    switch (o) {
+      case '+':
+      case '-':
+      case '/':
+      case 'x':
+        return true;
+      default:
+        return false;
+    }
+  }
+}
+
+export class Calculator {
+  private _oldValue: number = undefined;
+  private _newValue: number = undefined;
+  private _operator: string = "";
+
+  toString() {
+    if (this._operator.length == 0) {
+      return String(this._oldValue);
+    }
+    else if (this._operator.length > 0 && this._newValue == undefined) {
+      return this._oldValue + ' ' + this._operator;
+    }
+    else {
+      return this._oldValue + ' ' + this._operator + ' ' + this._newValue;
+    }
+  }
+
+  clear() {
+    this._oldValue = undefined;
+    this._newValue = undefined;
+    this._operator = "";
+    this.setValue(0);
+  }
+
+  setValue(v) {
+    if (this._operator.length == 0 ) {
+        this._oldValue = (this._oldValue == undefined || this._oldValue == 0) ? v : this._oldValue + v;
+    }
+    else  {
+        this._newValue = (this._newValue == undefined || this._newValue == 0) ? v : this._newValue + v;
+    }
+  }
+
+  setOperator(v) {
+    this._operator = v;
+  }
+
+  calculate() {
+    if (this._oldValue == undefined || this._newValue == undefined || this._operator.length == 0) {
+        return 0;
+    }    
+    
+    switch (this._operator) {
+      case '+':
+        this._oldValue = Number(this._oldValue) +  Number(this._newValue);
+        break;
+      case '-':
+        this._oldValue -= this._newValue;
+        break;
+      case '/':
+        this._oldValue /= this._newValue;
+        break;
+      case 'x':
+        this._oldValue *= this._newValue;
+        break;
+    }
+    this._newValue = undefined;
+    this._operator = "";
+    
+    return this._oldValue;
   }
 }
